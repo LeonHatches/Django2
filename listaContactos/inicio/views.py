8b4fbec89416cb19d8a3931fe1ea4c8f05c21070
leonhatches <jhatches@unsa.edu.pe>
2025-05-29 23:08:13 -0500

    Se crean las vistas (anotherView y hiddenView) para probar

--- listaContactos/inicio/views.py
+++ listaContactos/inicio/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def myHomeView(*args, **kwargs):
     return HttpResponse('<h1>Hola Mundito desde Django</h1>')
+
+def anotherView(request):
+    return HttpResponse('<h1>Esta es la otra página :)</h1>')
+
+def hiddenView(request):
+    return HttpResponse('<h1>Esta página esta oculta...</h1>')

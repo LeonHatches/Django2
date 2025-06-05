14173f65b063ea03c2815bd673aaa13bf6286327
leonhatches <jhatches@unsa.edu.pe>
2025-06-05 16:47:24 -0500

    Se crea la view para crear a la Persona con el objeto de nuestra clase Form y se crea el template

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Persona
+from .forms import PersonaForm
 
 # Create your views here.
 def personaTestView(request):
@@ -9,3 +10,15 @@ def personaTestView(request):
     }
 
     return render(request, "personas/descripcion.html", context)
+
+def personaCreateView(request):
+    form = PersonaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = PersonaForm()
+
+    context = {
+        'form': form
+    }
+
+    return (request, 'personas/personasCreate.html', context)

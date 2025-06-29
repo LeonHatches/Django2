93e7439fe2e6e16c63e36b42e7b518e2320384a5
leonhatches <jhatches@unsa.edu.pe>
2025-06-28 20:04:42 -0500

    Se modifican los campos y se crea un método para manejar enlaces dinámicos

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Persona (models.Model):
-    nombres   = models.TextField()
-    apellidos = models.TextField()
-    edad      = models.TextField()
+    nombres   = models.CharField()
+    apellidos = models.CharField()
+    edad      = models.IntegerField()
     menor     = models.BooleanField()
+
+    def get_absolute_url (self):
+        return "/personas/"+str(self.id)+"/"
 
     def __str__(self):
         return self.nombres

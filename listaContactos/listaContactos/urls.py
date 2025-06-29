6b3d28ce15701fb7b752633090658c4be150e67b
leonhatches <jhatches@unsa.edu.pe>
2025-06-28 19:42:27 -0500

    Se crea la ruta para visualizar la página de lista de personas

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView, anotherView, hiddenView
-from personas.views import personaTestView, personaCreateView, searchForHelp
+from personas.views import personaTestView, personaCreateView, searchForHelp, personaListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +27,5 @@ urlpatterns = [
     path('persona/', personaTestView, name = 'otro'),
     path('agregar/', personaCreateView, name = 'createPersona'),
     path('search/', searchForHelp, name = 'buscar'),
+    path('personas/', personaListView, name = 'Listing')
 ]

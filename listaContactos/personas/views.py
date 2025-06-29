f8d7cf9980b0727b4820de35ab47fe8dde1c40d4
leonhatches <jhatches@unsa.edu.pe>
2025-06-28 19:58:40 -0500

    Se crea un redirect para evitar caer en un error 404

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Persona
 from .forms import PersonaForm
 
@@ -42,6 +42,7 @@ def personaDeleteView(request, myID):
     if request.method == 'POST':
         print("Se borro")
         obj.delete()
+        return redirect('../../')
     
     context = {
         'objecto': obj,

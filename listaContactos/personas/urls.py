941176419b7891879965597de7fbf389caf25562
leonhatches <jhatches@unsa.edu.pe>
2025-06-28 21:22:09 -0500

    Se crea la vista que puede modificar objectos Persona

--- listaContactos/personas/urls.py
+++ listaContactos/personas/urls.py
@@ -8,10 +8,12 @@ from .views import (
     personasShowObject,
     personaDetailView,
     personaCreateView,
+    personaUpdateView,
 )
 
 app_name = 'personas'
 urlpatterns = [
+    path('<int:pk>/update/', personaUpdateView.as_view(), name="persona-update"),
     path('create/', personaCreateView.as_view(), name = 'persona-create'),
     path('', personaListView.as_view(), name = 'Listing'),
     path('<int:pk>/', personaDetailView.as_view(), name='persona-detail'),
--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Persona
 from .forms import PersonaForm
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 # Create your views here.
 def personaTestView(request):
@@ -58,6 +58,15 @@ class personaDetailView (DetailView):
     model = Persona
 
 class personaCreateView (CreateView):
+    model = Persona
+    fields = [
+        'nombres',
+        'apellidos',
+        'edad',
+        'menor'
+    ]
+
+class personaUpdateView (UpdateView):
     model = Persona
     fields = [
         'nombres',
